282b8bba210915798ef4dca9a283db8577f752e4
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2022-10-08 22:23:28 +0200

    server and get

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,16 @@ const {MongoClient} = require("mongodb")
 const app = express()
 let db
 
-
-
-
+//Get server and display allaAnnonser in the console. 
 app.get("/", async (req, res) => {
     const allaAnnonser = await db.collection("ads").find().toArray()
     console.log(allaAnnonser)
-    res.send("welcome")
+    res.send(`${allaAnnonser.map(ads => `<p>${ads.mat}</p>`)}`)
+})
+
+app.get("/annonser", async (req, res) => {
+    const allaAnnonser = await db.collection("ads").find().toArray()
+    res.json(allaAnnonser)
 })
 
 async function start() {
--- src/components/Annonser/Annonser.js
+++ src/components/Annonser/Annonser.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+
+
 
 function Annonser() {
+  // const annonser = [
+  //  { mat: "potatis",
+  //     allerg: "bajs",
+  //   info: "mumsigt"
+  // }]
+  const [annonser, setAnnonser] = useState([])
+  
+  useEffect(() => {
+    async function go() {
+      const response = await Axios.get("http://localhost:3005/annonser")
+      setAnnonser(response.data)
+    }
+    go()
+  }, [])
+  
   return (
     <div>
       <h1>
         Annonser
+        {annonser.map(function(annons) {
+          return <p><FoodCard mat={annons.mat} allerg={annons.allerg}info={annons.info} /></p>
+        })}
       </h1>
     </div>
   )
 }
-
+function FoodCard(props) {
+  return <p>Jag bakar {props.mat} 
+  och är allergisk mot {props.allerg} 
+  och tycker det är {props.info}. </p>
+}
 export default Annonser
