466f99d4bb5423ad99daa0f86d75e976c82db01b
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2022-11-17 12:20:56 +0100

    css changes

--- src/components/Profil/Profil.js
+++ src/components/Profil/Profil.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import bgpic from '../../assets/Images/ellipse.svg'
+import profilePic from '../../assets/Images/profile-pic.svg'
 
 function Profil() {
   return (
     <div>
-        <h1>Profil?</h1>
+        <div className='profil-top'>
+          <div className='bg-pic'>
+            <img src={bgpic} alt='green background'/>
+          </div>
+          <div className='profil-pic'>
+            <img src={profilePic} />
+          </div>
+        </div>
     </div>
   )
 }
